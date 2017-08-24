f5dad5f8a3d3fa90f68b2eca119d4aa54b96eab7
root <szlixiongsheng@163.com>
2017-08-24 11:41:34 +0800

    update

---
+++ protocol_uart.c
@@ -0,0 +1,106 @@
+﻿/*******************************************************************************
+
+    This file is part of the up2p.
+    Copyright wilddog.com
+    All right reserved.
+
+    File:    protocol_uart.c
+
+    No description
+
+    TIME LIST:
+    CREATE  skyli    
+
+*******************************************************************************/
+
+#include "protocol_uart.h"
+#include <stdio.h>
+#include <stdlib.h>
+/* todo
+* 从队列中取出数据，并解析
+**/
+int uart_parse(u8 *p_pack,const u16 len){
+	UART_PACKET *p_rec = NULL;
+	if(len < sizeof(UART_PACKET)){
+		// no match
+		return -1;
+	}
+	p_rec = (UART_PACKET*)p_pack;
+	if( p_rec->version != UART_VERSION){
+		// no match dump the package
+		return -1;
+	}
+	return uart_cmd_handl(p_pack);
+}
+/* todo
+* 底层接收
+*/
+int _uart_recv(u8 *p_rec,u16 len){
+	//todo put packet to queue
+	uart_parse(p_rec,len);
+}
+int uart_pack_alloc(u8 **pp_packet,u16 cmd,const u8 *p_data,const u16 len){
+
+	
+	UART_PACKET *p_packet = malloc(sizeof(UART_PACKET)+len);
+	if(!p_packet)
+		return -1;
+	//protocol packet
+	p_packet->version = UART_VERSION;
+	p_packet->cmd = cmd;
+	p_packet->len = len;
+
+	
+	if(!p_data | len ){
+		memcpy(p_packet->payload,p_data,len);
+
+		
+	}
+	
+	*pp_packet = p_packet;
+
+	
+	return (sizeof(UART_PACKET)+len);
+}
+
+// 响应串口数据
+int uart_cmd_handl(u8 *p_rec){
+	UART_PACKET *p_pack = (UART_PACKET*)p_rec;
+
+	switch(p_pack->cmd){
+		case CMD_SOCKET_ON:
+
+			break;
+		case CMD_SOKET_OFF:
+
+			break;
+		case CMD_LIGHT_UP:
+		{
+			UART_Light_T *p_light = (UART_Light_T*)p_pack->payload;
+			printf("change %d light to %d \n",p_light->no,p_light->intensity);
+		}
+		default: 
+			break;
+	}
+	return 0;
+}
+void main(void)
+{
+	UART_Light_T test_light;
+	u8 *p_send = NULL;
+	test_light.no = 10;
+	test_light.intensity = 99;
+	
+	int slen = uart_pack_alloc(&p_send,CMD_SOCKET_ON,&test_light,sizeof(UART_Light_T));
+
+	if(!slen | !p_send )
+		return -1;
+
+	_uart_recv(p_send,slen);
+
+	
+	free(p_send);
+	p_send = NULL;
+	
+
+}
---
+++ protocol_uart.h
@@ -0,0 +1,78 @@
+﻿/*******************************************************************************
+
+    This file is part of the up2p.
+    Copyright showhome
+    All right reserved.
+
+    File:    protocol_uart.h
+
+    No description
+
+    TIME LIST:
+    CREATE  skyli  
+
+*******************************************************************************/
+#ifndef _protocol_uart_h_
+#define _protocol_uart_h_
+
+#ifdef      __cplusplus
+extern "C" {
+#endif
+
+#define UART_VERSION	0x55
+
+#if __x86_64__ || __LP64__
+#pragma message("In 64Bit machine \n")
+
+typedef char s8;
+typedef unsigned char u8;
+typedef short s16;
+typedef unsigned short u16;
+typedef int s32;
+typedef unsigned int u32;
+
+#else
+
+#pragma message("In 32Bit machine \n")
+
+// 基本类型
+typedef char s8;
+typedef unsigned char u8;
+typedef short s16;
+typedef unsigned short u16;
+typedef long s32;
+typedef unsigned long u32;
+
+#endif
+
+#define UART_VERSION	0x55
+
+// 指令类型
+typedef enum UART_CMD_T{
+    // 不加密命令
+    CMD_NONE                = 0x00,   // 空类型
+	CMD_SOCKET_ON			= 0x10,   //控制开关
+	CMD_SOKET_OFF			= 0x11,	
+    CMD_COLOR_SET			= 0x20,	// 控制发送串口
+	
+	CMD_MAX
+}UART_Cmd_T;
+    
+	
+typedef struct{
+    u16 version;          // 验证数,表示协议和协议版本
+    u16 cmd;            // 命令类型 
+    u16 idx;			// 序号, 预留给 sensor 使用
+	u16 len;            // 数据长度
+    char payload[0];    // 数据
+} UART_PACKET;
+typedef struct{
+	u8 no;		  // 几号灯
+	u8 intensity; // 光强度
+}UART_Light_T;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif _protocol_uart_h_
